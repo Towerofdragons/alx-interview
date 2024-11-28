6af9c853d195dcb4d2b561a8e55bcc12d80f711f
Mark <marknjoropge@gmail.com>
2024-11-28 19:40:33 +0000

    Task 0, greedy approach

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#/usr/bin/python3
+"""
+Use Greedy approach to find least number of coins.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Use Greedy approach to find least number of coins.
+    """
+    # If total less than or eq to 0
+    if total <= 0:
+        return 0
+
+    # Sort list of coins to lead with the highest
+    coins.sort(reverse=True)
+    coin_num = 0
+
+    # Loop over each coin
+    for coin in coins:
+        if total == 0:
+            break
+
+        # Find how many of current coin can fit in total
+        coin_num += total // coin
+
+        # Find remainder and set total to remainder after coins used
+        total %= coin
+
+    return coin_num if total == 0 else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+## 0x08-making_change
+
+A classic problem from the domain of dynamic programming and greedy algorithms: the coin change problem. The objective is to find the minimum number of coins required to make up a given total amount, given a list of coin denominations. 
