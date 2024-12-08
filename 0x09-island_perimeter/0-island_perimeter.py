cedaf3dadc3a63707aab56626102a57cfec5bbe1
Mark <marknjoropge@gmail.com>
2024-12-08 19:45:53 +0000

    Correct code to pep8 formatting

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,20 +2,28 @@
 
 """
 Calculate the perimeter of the island described in the grid.
+
+grid is a list of list of integers:
+0 represents water
+1 represents land
+Each cell is square, with a side length of 1
+Cells are connected horizontally/vertically (not diagonally).
+grid is rectangular, with its width and height not exceeding 100
 """
+
 
 def island_perimeter(grid):
     """
         Find the primeter of the island
     """
-    rows =len(grid)
-    columns =  len(grid[0])
+    rows = len(grid)
+    columns = len(grid[0])
 
     perimeter = 0
 
     for i in range(rows):
         for j in range(columns):
-            if grid[i][j] == 1: # If the cell is a land cell
+            if grid[i][j] == 1:  # If the cell is a land cell
                 # Start with 4 sides
                 cell_perimeter = 4
 
